b3d116affdfb9c50bf0725a45bdd46235c76fb0a
Rithvik-2352 <rithvikdilip@outlook.com>
2026-02-11 20:10:32 -0800

    Add files via upload

--- quiz game.py
+++ quiz game.py
@@ -11,6 +11,27 @@ r_6=Rect (400,430,300,200)
 r_7=Rect (750,200,100,200)
 r_8=Rect (750,430,100,200)
 timeleft=10
+question=[]
+questions=[]
+filepath = "questions.txt"
+question_count=0
+question_index = 0
+def read_question_file():
+    global questions
+    global question_count
+    file = open (filepath,'r')
+    for content in file:
+        questions.append (content.strip())
+        question_count += 1
+    file.close ()
+read_question_file()
+print (questions)
+def read_question():
+    global question_index, question, question_count
+    if question_index<question_count:
+        question = questions.pop(question_index).split ("|")
+read_question()
+print (question)
 def draw ():
     global timeleft
     screen.fill("black")
@@ -29,5 +50,19 @@ def draw ():
     screen.draw.filled_rect (r_8,(255,0,0))
     screen.draw.textbox (("Reset"), r_8, color = (0,0,0), angle=-90)
 def update():
-    pass
+    r.x -=2
+    if r.right <200:
+        r.left=WIDTH
+def update_time():
+    global timeleft
+    if timeleft>0:
+        timeleft-=1
+    else:
+        gameover()
+def gameover():
+    global question
+    global timeleft
+    question = ["Game Over!","-","-","-","-",5]
+    timeleft = 0
+clock.schedule_interval (update_time,1)
 pgzrun.go()
